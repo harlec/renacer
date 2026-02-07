8f5dd788cdfc23ccb7e1734f517082c488b11707
hugoromero <dhugors@gmail.com>
2026-02-07 09:01:27 -0500

    borrar_compra

--- inc/borrar_compra.php
+++ inc/borrar_compra.php
@@ -21,36 +21,20 @@ $mensajeError = 'hasta aca bien';
 
 	foreach ($vdl as $value) {
 		$producto = $value['producto'];
-		$fv = $value['fv'];
 
 		$stock = Sdba::table('stock');
 		$stock->where('producto',$producto);
 		$stock->order_by('id_stock','desc');
 		$stockl = $stock->get_one();
-		$cstock = $stockl['stockt'];
+		$cstock = isset($stockl['stockt']) ? (int)$stockl['stockt'] : 0;
 		$nstockt = $cstock - $value['ingreso']; //stock total x producto
 		$motivo = 'EC-'.$id;
-
-		//obtenemos el stock del producto x lote
-		$stock->reset();
-		$stock->where('producto',$producto)->and_where('fv =',$fv);
-		$stock->order_by('id_stock','desc');
-		$stockl = $stock->get_one();
-		$cstock = $stockl['stock'];
-		$nstock = $cstock - $value['ingreso'];
-
-
-		$datas = array('id_stock'=>'','producto'=>$producto,'egreso'=>$value['ingreso'],'motivo'=>$motivo,'stock'=>$nstock,'fv'=>$fv,'stockt'=>$nstockt,'fecha'=>$fecha, 'estado'=>'0');
+		$datas = array('id_stock'=>'','producto'=>$producto,'egreso'=>$value['ingreso'],'motivo'=>$motivo,'stock'=>$nstockt,'fv'=>'','stockt'=>$nstockt,'fecha'=>$fecha, 'estado'=>'0');
 		$stock->insert($datas);
 
-
-		//actualizamos la variacion
-		$variacion = Sdba::table('variantes');
-		$variacion->where('producto',$producto)->and_where('variante',$fv);
-		$vr = $variacion->get_one();
-		$idvr = $vr['id_variante'];
-		$datava = array('id_variante'=>$idvr,'producto'=>$producto,'variante'=>$fv, 'stock'=>$nstock);
-		$variacion->set($datava);
+		$productos = Sdba::table('productos');
+		$productos->where('id_producto',$producto);
+		$productos->update(array('stockp'=>$nstockt));
 		
 	}
 
