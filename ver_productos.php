9200ea30055382581be17ecc67eb76aeff2523c6
hugoromero <dhugors@gmail.com>
2026-08-15 15:59:34 -0500

    pdf

--- ver_productos.php
+++ ver_productos.php
@@ -157,7 +157,10 @@ foreach ($ventas_list as $value) {
 	<script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
   <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
   <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 	<script >
 	// A $( document ).ready() block.
 	$(document ).ready(function() {
@@ -215,7 +218,38 @@ foreach ($ventas_list as $value) {
 		    }           
 		} );     
 
-		$('#datos').DataTable();	
+		function nombreArchivoProductos() {
+			var fecha = new Date().toISOString().slice(0, 10);
+			return 'productos_' + fecha;
+		}
+
+		function tituloReporteProductos() {
+			var fecha = new Date().toLocaleDateString('es-PE');
+			var filtro = tablaProductos.search();
+			var titulo = 'Listado de productos - ' + fecha;
+			if (filtro) titulo += ' - Filtro: "' + filtro + '"';
+			return titulo;
+		}
+
+		var tablaProductos = $('#datos').DataTable({
+			dom: 'Bfrtip',
+			buttons: [
+				{
+					extend: 'excel',
+					text: 'Excel',
+					title: tituloReporteProductos,
+					filename: nombreArchivoProductos,
+					exportOptions: { columns: ':not(:last-child)' }
+				},
+				{
+					extend: 'pdf',
+					text: 'PDF',
+					title: tituloReporteProductos,
+					filename: nombreArchivoProductos,
+					exportOptions: { columns: ':not(:last-child)' }
+				}
+			]
+		});
 	    console.log( "ready!" );
 
 	    $('body').on('click',"#borrar", function() {
